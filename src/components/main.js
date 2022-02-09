074614df759a492dc7d2c234d9273d94e9151f22
oroskulovermat <oroskulovermat@gmail.com>
2022-02-09 16:32:34 +0600

    initial

---
+++ src/components/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            <div className='container mx-auto'>
+                <div className=' grid grid-cols-3'>
+                    <div className='bg-red-400 w-3/4  h-28 center' >ertgrf</div>
+                    <div className='bg-green-900  w-3/4 h-28'>ertgrf</div>
+                    <div className='bg-red-400 w-3/4 h-28'>ertgrf</div>
+                    <div className='bg-red-400 w-3/4 h-28 my-5'>ertgrf</div>
+
+                </div>
+
+            </div>
+
+        </main>
+    );
+};
+
+export default Main;
